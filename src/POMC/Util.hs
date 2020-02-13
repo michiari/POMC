18f81b2225f7bec55517b0185fb0337350b2ffaf
daberg <davide.bergamaschi@protonmail.com>
2020-02-13 18:16:30 +0000

    Remove useless func

--- src/POMC/Util.hs
+++ src/POMC/Util.hs
@@ -1,12 +1,6 @@
-module POMC.Util ( trace
-                 , unsafeLookup
+module POMC.Util ( unsafeLookup
                  , lookupOrDefault
                  ) where
-
-import qualified Debug.Trace
-
-trace :: (Show a) => a -> a
-trace a = Debug.Trace.trace (show a) a
 
 unsafeLookup :: Eq a => a -> [(a, b)] -> b
 unsafeLookup k al = case lookup k al of
