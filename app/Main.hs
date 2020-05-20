32394ca3e1d9398a13fa1ecc4758e29ca51b9bd0
daberg <davide.bergamaschi@protonmail.com>
2020-05-21 01:07:50 +0200

    Add first app implementation
    Currently reads a list of formulas and a list of strings from the
    specified input file; for every formula f and every string s, it checks
    whether s satisfies f and prints the result.

--- app/Main.hs
+++ app/Main.hs
@@ -1,2 +1,35 @@
+import POMC.Example (stlPrecedenceV2)
+import POMC.Check (fastcheck)
+import POMC.Parse (checkRequestP, spaceP, CheckRequest(..)) 
+import POMC.Util (safeHead)
+
+import Prelude hiding (readFile)
+
+import System.Environment
+import System.Exit
+
+import Text.Megaparsec
+import Data.Text.IO (readFile)
+
+import Control.Monad
+
+exitHelp :: IO a
+exitHelp = do progName <- getProgName
+              die ("USAGE:    " ++ progName ++ " FILE")
+
 main :: IO ()
-main = error "Not implemented"
+main = do args <- getArgs
+          fname <- case safeHead args of
+                     (Just "--help") -> exitHelp
+                     (Just fname)    -> return fname
+                     Nothing         -> exitHelp
+          fcontent <- readFile fname
+          checkReq <- case parse (spaceP *> checkRequestP <* eof) fname fcontent of
+                        Left  errBundle -> die (errorBundlePretty errBundle)
+                        Right checkReq  -> return checkReq
+          forM [ (f,s,fastcheck f stlPrecedenceV2 s) | f <- creqFormulas checkReq, s <- creqStrings checkReq]
+               (putStrLn . showResult)
+          return ()
+  where showResult (f, s, r) = "\nFormula: " ++ show f ++
+                               "\nString:  " ++ show s ++
+                               "\nResult:  " ++ show r 
