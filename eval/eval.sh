5c776abb189a35101ed8a45894c74f8e45f8fd3f
Michele Chiari <michele.chiari@polimi.it>
2020-05-23 12:55:48 +0200

    Added some tests for artifact evaluation.

---
+++ eval/eval.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    TESTS=(./*.pomc)
+else
+    TESTS=("$@")
+fi
+
+for F in "${TESTS[@]}"; do
+    echo "_______________________________________________________________________________"
+    echo "Evaluating file "$F" ..."
+    time stack exec pomc -- "$F"
+done
