a22ba051a861d3adb2dea2a859dce064e701e08a
Michele Chiari <michele.chiari@polimi.it>
2020-09-30 16:20:09 +0200

    eval directory refactored to add model checking tests.

--- eval/eval.sh
+++ eval/eval.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+function run_test {
+    echo "_______________________________________________________________________________"
+    echo "Evaluating file "$1" ..."
+    stack exec pomc -- "$1"
+}
+
 if [ $# -eq 0 ]; then
-    TESTS=(./*.pomc)
+    TESTS=($(find . -name "*.pomc"))
 else
     TESTS=("$@")
 fi
 
-for F in "${TESTS[@]}"; do
-    echo "_______________________________________________________________________________"
-    echo "Evaluating file "$F" ..."
-    stack exec pomc -- "$F"
+for T in "${TESTS[@]}"; do
+    if [ -f $T ]; then
+        run_test $T
+    elif [ -d $T ]; then
+        for F in $(find $T -name "*.pomc"); do
+            run_test $F
+        done
+    fi
 done
