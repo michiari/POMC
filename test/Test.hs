97f4aaeeba4e77695bdbb8e5b3d56e81aefa56de
Michele Chiari <michele.chiari@polimi.it>
2021-01-11 11:09:16 +0100

    Enable all tests.

--- test/Test.hs
+++ test/Test.hs
@@ -3,10 +3,10 @@ import Test.Tasty
 import qualified TestOpa   (tests)
 import qualified TestCheck (tests)
 import qualified TestSat   (tests)
-import qualified TestMC   (tests)
+import qualified TestMC    (tests)
 
 main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [ TestCheck.tests]
+tests = testGroup "Tests" [TestOpa.tests, TestCheck.tests, TestSat.tests, TestMC.tests]
