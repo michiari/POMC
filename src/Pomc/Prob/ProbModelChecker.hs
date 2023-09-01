d72783e105ccc636ba227ca500bfd2ca1ea9a43b
Francesco Pontiggia <francesco.pontiggia@mail.polimi.it>
2023-09-01 15:59:42 +0200

    add terminationExplicit: it computes the summary chain of a popa and calls the Z3 Encoding to compute its termination probaiblity

--- src/Pomc/Prob/ProbModelChecker.hs
+++ src/Pomc/Prob/ProbModelChecker.hs
@@ -9,17 +9,27 @@
 
 module Pomc.Prob.ProbModelChecker ( Popa(..)
                                   , ExplicitPopa(..)
+                                  , terminationExplicit
                                   ) where
-
-#define NDEBUG
 
 import Pomc.Prop (Prop(..))
 import Pomc.Prec (Alphabet)
 import qualified Pomc.Encoding as E
 import Data.Set (Set)
+import qualified Data.Set as Set 
 
-import Pomc.Prob.Termination(terminationProbs)
+import qualified Data.Map as Map
+
+import Pomc.Check(makeOpa)
+import Pomc.PropConv ( convProps, PropConv(encodeProp) )
+
+import Pomc.Prob.SummaryChain(decomposeGraph, ProbDelta)
+import qualified Pomc.Prob.SummaryChain as SC
+import Pomc.Prob.Z3Termination(terminationQuery)
 import Pomc.Prob.ProbUtils(Prob, RichDistr, Label)
+import Data.Hashable
+import Pomc.Potl (Formula(T))
+import Control.Monad.ST (stToIO)
 
 data Popa s a = Popa
   { alphabet       :: Alphabet a -- OP alphabet
@@ -30,6 +40,7 @@ data Popa s a = Popa
   , labelling      :: s -> Set (Prop a) -- labelling function
   }
 
+-- TODO: add normalize RichDistr to optimize the encoding
 data ExplicitPopa s a = ExplicitPopa
   { epAlphabet       :: Alphabet a -- OP alphabet
   , epInitial        :: (s, Set (Prop a)) -- initial state of the POPA
@@ -37,3 +48,45 @@ data ExplicitPopa s a = ExplicitPopa
   , epopaDeltaShift  :: [(s, RichDistr s (Set (Prop a)))] -- shift transition prob. distribution
   , epopaDeltaPop    :: [(s, s, RichDistr s (Set (Prop a)))] -- pop transition prob. distribution
   } deriving (Show)
+
+
+terminationExplicit :: (Ord s, Hashable s, Show s, Ord a)
+                    => ExplicitPopa s a
+                    -> Prob
+                    -> IO (Maybe Rational)
+terminationExplicit popa prob = 
+  let 
+    (sls, prec) = epAlphabet popa
+    (_, tprec, [tsls], pconv) = convProps T prec [sls]
+    
+    (bitenc, precFunc, _, _, _, _, _, _) =
+      makeOpa T False (tsls, tprec) (\_ _ -> True)
+
+    maybeList Nothing = []
+    maybeList (Just l) = l
+    
+    -- generate the delta relation of the input opa
+    encodeDistr = map (\(s, b, p) -> (s, E.encodeInput bitenc (Set.map (encodeProp pconv) b), p))
+    makeDeltaMapI delta = Map.fromList $
+      map (\(q, distr) -> (q, encodeDistr  distr))
+          delta
+    deltaPush  = makeDeltaMapI  (epopaDeltaPush popa)
+    deltaShift  = makeDeltaMapI  (epopaDeltaShift popa)
+    popaDeltaPush  q = maybeList $ Map.lookup q (deltaPush ) 
+    popaDeltaShift  q = maybeList $ Map.lookup q (deltaShift  )
+
+    makeDeltaMapS  delta = Map.fromList $
+      map (\(q, q', distr) -> ((q, q'), encodeDistr  distr))
+          delta
+    popaDeltaPop  q q' = maybeList $ Map.lookup (q, q') $ makeDeltaMapS   (epopaDeltaPop popa) 
+
+    pDelta = SC.Delta
+            { SC.bitenc = bitenc
+            , SC.prec = precFunc
+            , SC.deltaPush = popaDeltaPush
+            , SC.deltaShift = popaDeltaShift
+            , SC.deltaPop = popaDeltaPop
+            }
+  in do 
+    sc <- stToIO $ decomposeGraph pDelta (fst . epInitial $ popa) (E.encodeInput bitenc . Set.map (encodeProp pconv) . snd .  epInitial $ popa)
+    terminationQuery sc precFunc prob
