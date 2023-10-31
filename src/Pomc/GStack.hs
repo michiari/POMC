ce04b9a0741733101f7e15214d3a7e70deaa4967
Francesco Pontiggia <francesco.pontiggia@mail.polimi.it>
2023-10-31 16:51:35 +0100

    updata GStack data structure - delete unused functions, add new ones,  fix the semantics of some of them

--- src/Pomc/GStack.hs
+++ src/Pomc/GStack.hs
@@ -6,20 +6,21 @@
 -}
 
 module Pomc.GStack ( GStack
+                   , new
                    , push
                    , peek
+                   , safePeek
+                   , multPeek
                    , pop
                    , pop_
                    , size
-                   , new
                    , multPop
-                   , popWhile
                    , popWhile_
                    ) where
 
 import Control.Monad.ST (ST)
-import Control.Monad(replicateM)
 import Data.STRef (STRef, newSTRef, readSTRef, writeSTRef, modifySTRef')
+import Data.Maybe (listToMaybe)
 
 -- an implementation for a stack 
 -- needed in the Gabow algorithm
@@ -36,11 +37,18 @@ push (gsref, lenref) val = do
   modifySTRef' gsref $ \l -> val:l;
   modifySTRef' lenref (+1)
 
-peek :: GStack s v -> ST s v
-peek (gsref, _)  = do
-  gs <- readSTRef gsref
-  return $ head gs
+safePeek :: GStack s v -> ST s (Maybe v)
+safePeek (gsref, _)  = listToMaybe <$> readSTRef gsref
 
+-- requires: the stack must be non empty
+peek :: GStack s v -> ST s v
+peek (gsref, _)  = head <$> readSTRef gsref
+
+-- requires: the stack must contain at leat n elements
+multPeek :: GStack s v -> Int -> ST s [v]
+multPeek (gsref, _) n = take n <$> readSTRef gsref
+
+-- requires: the stack must be non empty
 pop :: GStack s v -> ST s v
 pop (gsref, lenref)  = do
   gs <- readSTRef gsref
@@ -48,32 +56,17 @@ pop (gsref, lenref)  = do
   modifySTRef' lenref (+(-1))
   return $ head gs
 
+-- requires: the stack must be non empty
 pop_ :: GStack s v -> ST s ()
 pop_ (gsref, lenref)  = do
   gs <- readSTRef gsref
   writeSTRef gsref $ tail gs;
   modifySTRef' lenref (+(-1))
-  return ()
 
 size :: GStack s v -> ST s Int
 size (_, lenref) = readSTRef lenref
 
-multPop :: GStack s v -> Int  -> ST s [v]
-multPop gs n  = replicateM n $ pop gs
-
-popWhile :: GStack s v -> (v -> Bool) -> ST s [v]
-popWhile (gsref, lenref) cond = 
-  let 
-    recurse True  gs l acc = recurse (cond . head . tail $ gs) (tail gs) (l+(-1)) ((head gs):acc)
-    recurse False gs l acc = do 
-      writeSTRef gsref gs 
-      writeSTRef lenref l 
-      return acc
-  in do 
-    gs <- readSTRef gsref
-    l <- readSTRef lenref
-    recurse (cond $ head gs) gs l [] 
-
+-- requires: the condition must hold before reaching the bottom of the stack
 popWhile_ :: GStack s v -> (v -> Bool) -> ST s ()
 popWhile_ (gsref, lenref) cond = 
   let 
@@ -86,3 +79,11 @@ popWhile_ (gsref, lenref) cond =
     gs <- readSTRef gsref
     l <- readSTRef lenref
     recurse (cond $ head gs) gs l 
+
+-- ensures: the returned list respects the previous order on the stack (first element of the returned list = top stack element)
+multPop :: GStack s v -> Int  -> ST s [v]
+multPop (gsref, lenref) n = do 
+  (prefix, remainder) <- splitAt n <$> readSTRef gsref 
+  writeSTRef gsref remainder 
+  modifySTRef' lenref (+(-n))
+  return prefix
