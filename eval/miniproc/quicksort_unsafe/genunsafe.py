80a360e1cee97bc3fbb21108083ca97e00b0b4f1
Michele Chiari <michele.chiari@polimi.it>
2023-11-29 17:21:21 +0100

    Fix new quicksort benchmark

--- eval/miniproc/quicksort_unsafe/genunsafe.py
+++ eval/miniproc/quicksort_unsafe/genunsafe.py
@@ -26,7 +26,7 @@ def load_prog_unsafe():
 
 def gen_bench(base_path, filename_template, formulas, prog_template, width, size):
     init = '\n'.join({'a[{:d}s6] = *;'.format(n) for n in range(size)})
-    comp = ' && '.join({'a[{:d}s6] <= a[{:d}s6]'.format(n, n + 1) for n in range(0, size-1)})
+    comp = ' && '.join({'a[{:d}s6] <= a[{:d}s6]'.format(n, n + 1) for n in range(0, size-1)}) if size > 1 else 'true'
     content = bench_template.format(spec=',\n'.join(formulas),
                                     program=prog_template.format(width=width, size=size, init=init, comp=comp))
     filepath = path.join(base_path, filename_template.format(width=width, size=size))
@@ -34,11 +34,11 @@ def gen_bench(base_path, filename_template, formulas, prog_template, width, size
         outfile.write(content)
 
 def gen_suite(base_path, filename_template, formulas, prog_template, width_from, width_to, size_from, size_to):
-    for width in range(width_from, width_to + 1):
-        wpath = path.join(base_path, 'u{:d}'.format(width))
-        makedirs(wpath, mode=0o775, exist_ok=True)
-        for size in range(size_from, size_to + 1):
-            gen_bench(wpath, filename_template, formulas, prog_template, width, size)
+    for size in range(size_from, size_to + 1):
+        spath = path.join(base_path, 'size{:d}'.format(size))
+        makedirs(spath, mode=0o775, exist_ok=True)
+        for width in range(width_from, width_to + 1):
+            gen_bench(spath, filename_template, formulas, prog_template, width, size)
 
 if __name__ == '__main__':
     unsafe_template = load_prog_unsafe()
