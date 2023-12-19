c152e0b44b6c3dab7c5ebf5cd6b59f12716c343b
Michele Chiari <michele.chiari@polimi.it>
2023-12-19 22:02:55 +0100

    Implement power iteration.

--- src/Pomc/Prob/OVI.hs
+++ src/Pomc/Prob/OVI.hs
@@ -19,12 +19,14 @@ import qualified Data.HashTable.ST.Basic as BHT
 import Data.Vector.Mutable (IOVector)
 import qualified Data.Vector.Mutable as MV
 
+import qualified Debug.Trace as DBG
+
 data OVISettings n = OVISettings { oviMaxIters :: Int
                                  , oviMaxKleeneIters :: Int
                                  , oviKleeneEps :: n
                                  , oviKleeneDampingFactor :: n
                                  , oviPowerIterEps :: n
-                                 , oviPowerIterDumpingFactor :: n
+                                 , oviPowerIterDampingFactor :: n
                                  , oviMaxPowerIters :: Int
                                  }
 
@@ -50,6 +52,19 @@ evalMonomial m v = liftIO $ case m of
     return $ c * v1
   Const c -> return c
 
+evalPolynomial :: (MonadIO m, Num n) => Polynomial2 n -> ProbVec n -> m n
+evalPolynomial p v = liftIO $ sum <$> mapM (flip evalMonomial v) p
+
+evalPolySys :: (MonadIO m, Ord n, Fractional n)
+            => PolyVector n -> (Bool -> n -> n -> Bool) -> ProbVec n
+            -> m Bool
+evalPolySys polySys checkRes vec = liftIO $ MV.foldM' eval True polySys
+  where eval prevCheck (key, poly) = do
+          oldV <- fromJust <$> HT.lookup vec key
+          newV <- evalPolynomial poly vec
+          HT.insert vec key newV
+          return $ checkRes prevCheck newV oldV
+
 -- compute dm/dx
 monomialDerivative :: Num n => Monomial2 n -> VarKey -> Monomial2 n
 monomialDerivative m x = case m of -- ugly but it works
@@ -64,9 +79,8 @@ monomialDerivative m x = case m of -- ugly but it works
 -- compute (J|v + I) x, where J|v is the Jacobian of leqSys evaluated on v,
 -- I is the identity matrix, and x is the vector of all variables
 jacobiTimesX :: (MonadIO m, Num n) => LEqSys n -> ProbVec n -> m (PolyVector n)
-jacobiTimesX leqSys v = liftIO $ do
-  MV.generateM (MV.length leqSys)
-    (\i -> do
+jacobiTimesX leqSys v = liftIO $ MV.generateM (MV.length leqSys)
+  (\i -> do
         (k, eq) <- MV.read leqSys i
         poly <- case eq of
           PushLEq terms -> (Lin 1 k :) . concat <$> forM terms
@@ -84,27 +98,41 @@ jacobiTimesX leqSys v = liftIO $ do
         return (k, poly)
     )
 
-powerIterate :: (MonadIO m, Num n) => n -> Int -> PolyVector n -> ProbVec n -> m n
-powerIterate eps maxIters matrix eigenVec = error "TODO"
+powerIterate :: (MonadIO m, Fractional n, Ord n)
+             => n -> Int -> PolyVector n -> ProbVec n -> m n
+powerIterate eps maxIters matrix eigenVec = liftIO $ go 0 maxIters where
+  checkRes prevCheck newV oldV =
+    prevCheck && abs (newV - oldV) <= eps
+  go eigenVal 0 = return eigenVal
+  go _ iters = do
+    stop <- evalPolySys matrix checkRes eigenVec -- TODO: check if Gauss-Seidel update is OK
+    -- get approximate largest eigenvalue as the maxNorm
+    eigenVal <- HT.foldM (\oldMax (_, v) -> return $ max oldMax v) 0 eigenVec
+    -- normalize eigenVec on the largest eigenValue
+    -- FIXME: iterate on matrix because I don't know if I can modify a HT while iterating on it
+    MV.mapM_ (\(k, _) -> HT.mutate eigenVec k (\(Just v) -> (Just $ v / eigenVal, v))) matrix
+    if stop
+      then return eigenVal
+      else go eigenVal (iters - 1)
+
 
 computeEigen :: (MonadIO m, Fractional n, Ord n)
              => LEqSys n -> n -> Int -> ProbVec n -> ProbVec n -> m n
 computeEigen leqSys eps maxIters lowerApprox eigenVec = liftIO $ do
   matrix <- jacobiTimesX leqSys lowerApprox
   eigenVal <- powerIterate eps maxIters matrix eigenVec -- modifies eigenVec in-place
-  maxNorm <- HT.foldM (\oldMax (_, v) -> return $ max oldMax v) 0 eigenVec
-  -- normalize eigenVec on the maxNorm
-  -- FIXME: iterate on leqSys because I don't know if I can modify a HT while iterating on it
-  MV.mapM_ (\(k, _) -> HT.mutate eigenVec k (\(Just v) -> (Just $ v / maxNorm, v))) leqSys
   return $ eigenVal - 1 -- -1 because we added the identity matrix
+
 
 ovi :: (MonadIO m, Fractional n, Ord n, Show n)
     => OVISettings n -> EqMap n -> ProbVec n -> m (OVIResult n)
 ovi settings eqMap lowerApprox = liftIO $ do -- TODO: maybe make copy of lowerApprox?
+  DBG.traceM "Starting OVI..."
   leqSys <- toLiveEqMap eqMap
   upperApprox <- HT.newSized =<< (stToIO $ BHT.size lowerApprox)
   eigenVec <- HT.newSized $ MV.length leqSys
   -- we only use live equations for eigenVec to avoid too many 0 values
+  -- TODO: check if this is correct
   let
     go _ _ 0 = return OVIResult { oviSuccess  = False
                                 , oviIters = oviMaxIters settings
@@ -127,6 +155,7 @@ ovi settings eqMap lowerApprox = liftIO $ do -- TODO: maybe make copy of lowerAp
       -- check if upperApprox is inductive
       inductive <- evalEqSys leqSys (\prev newV oldV -> prev && newV <= oldV) upperApprox
 
+      DBG.traceM $ "Finished iteration " ++ show (oviMaxIters settings - maxIters) ++ ". Inductive? " ++ show inductive
       if inductive
         then return OVIResult { oviSuccess  = True
                               , oviIters = oviMaxIters settings - maxIters
@@ -134,7 +163,7 @@ ovi settings eqMap lowerApprox = liftIO $ do -- TODO: maybe make copy of lowerAp
                               } -- This is the upperApprox iterated once: is this ok?
         else go
              (kleeneEps * oviKleeneDampingFactor settings)
-             (powerIterEps * oviPowerIterDumpingFactor settings)
+             (powerIterEps * oviPowerIterDampingFactor settings)
              (maxIters - 1)
 
   go (oviKleeneEps settings) (oviPowerIterEps settings) (oviMaxIters settings)
