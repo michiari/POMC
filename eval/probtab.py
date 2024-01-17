bd22720c43aa9204b504e5f319fe1f3963f86f63
Michele Chiari <michele.chiari@polimi.it>
2024-01-17 12:20:12 +0100

    Update script for tables

--- eval/probtab.py
+++ eval/probtab.py
@@ -19,18 +19,19 @@ def load_data(ovi_files, z3_files):
         r['formula'] = tokens[-1].split('.')[0]
 
     z3_records = load_files(z3_files)
-    for r in z3_records:
+    for k, r in z3_records.items():
         if r['result'] not in ['True', 'False']:
-            ovi_records[r['name']]['z3_time'] = r['result']
+            ovi_records[k]['z3_time'] = r['result']
         else:
-            ovi_records[r['name']]['z3_time'] = r['time']
+            ovi_records[k]['z3_time'] = r['time']
     return ovi_records
 
 def to_list(records, key_map_list):
     return [[(mapf(r[key]) if key in r else '--') for key, mapf in key_map_list] for r in records.values()]
 
 def pretty_print(results, latex=False):
-    idf = lambda x: x ## TODO: remove idf if not used
+    idf = lambda x: x
+    to_ms = lambda t: float(t) * 1000
     key_map_list = [
         ('bench', idf),
         ('states', idf),
@@ -40,17 +41,18 @@ def pretty_print(results, latex=False):
         ('maxscc', idf),
         ('formula', idf),
         ('z3_time', idf),
-        ('ub_time', idf),
-        ('past_time', idf),
+        ('ub_time', to_ms),
+        ('past_time', to_ms),
         ('time', idf),
-        ('mem_tot', idf),
+        ('mem_tot', lambda m: float(m) / 2**20),
         ('result', idf),
     ]
-    results_matrix = to_list(results, key_map_list)
+    key_fun = lambda r: f'{r[0]}{int(r[6][1:]):02d}'
+    results_matrix = sorted(to_list(results, key_map_list), key=key_fun)
 
-    header = ["Benchmark", "|Q_A|", "|SG|", "|f|", "#SCC", "|SCC|max", "formula", "z3 Time", "OVI UB Time (s)", "OVI PAST Time (s)", f"OVI Time (s)", "Memory (KiB)", "Result"]
+    header = ["Benchmark", "|Q_A|", "|SG|", "|f|", "#SCC", "|SCC|max", "formula", "z3 Time", "OVI UB Time (ms)", "OVI PAST Time (ms)", f"OVI Time (s)", "Memory (GiB)", "Result"]
 
-    print(tabulate(results_matrix, headers=header, tablefmt="latex" if latex else "plain"))
+    print(tabulate(results_matrix, headers=header, tablefmt="latex" if latex else "plain", floatfmt=".2f"))
 
 
 def print_help(pname):
