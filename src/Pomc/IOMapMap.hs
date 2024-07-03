be40a8c5acec52de9451be53d636818eef86f12b
Francesco Pontiggia <francesco.pontiggia@mail.polimi.it>
2024-07-03 16:27:06 +0200

    update MapMap; add IOMapMap, its variant in the IO monad

--- src/Pomc/IOMapMap.hs
+++ src/Pomc/IOMapMap.hs
@@ -1,36 +1,41 @@
 {- |
-   Module      : Pomc.MapMap
-   Copyright   : 2023 Francesco Pontiggia
+   Module      : Pomc.IOIOMapMap
+   Copyright   : 2024 Francesco Pontiggia
    License     : MIT
    Maintainer  : Francesco Pontiggia
 -}
 
-module Pomc.MapMap ( MapMap
+module Pomc.IOMapMap ( IOMapMap
                    , insert
                    , insertWith
                    , lookup
+                   , lookupValue
                    , member
+                   , delete
                    , empty
-                   , showMapMap
+                   , showIOMapMap
+                   , foldMaps
                    ) where
 
 import Prelude hiding (lookup)
-import qualified Control.Monad.ST as ST
-import Data.STRef (STRef, newSTRef, readSTRef, writeSTRef)
 
 import qualified Data.Vector.Mutable as MV
 
 import qualified Data.Map as Map
 import Data.Map(Map)
+import Data.IORef (readIORef, IORef, newIORef, writeIORef)
+import Data.Vector.Mutable (IOVector)
+
+import Control.Monad(when)
 
 -- Map to Maps
-type MapMap s k v = MV.MVector s (Map k v)
+type IOMapMap k v = IOVector (Map k v)
 
--- insert a pair (key, value) into the MapMap
+-- insert a pair (key, value) into the IOMapMap
 -- ensures: it replaces the existing mapping, if existing
-insert :: (Ord k) => STRef s (MapMap s k v) -> Int -> k -> v -> ST.ST s ()
+insert :: (Ord k) => IORef (IOMapMap k v) -> Int -> k -> v -> IO ()
 insert mmref idx key val = do
-  mm <- readSTRef mmref
+  mm <- readIORef mmref
   let len = MV.length mm
   if idx < len
     then MV.unsafeModify mm (Map.insert key val) idx
@@ -40,14 +45,14 @@ insert mmref idx key val = do
          in do { grown <- MV.grow mm (newLen-len)
                ; mapM_ (\i -> MV.unsafeWrite grown i Map.empty) [len..(newLen-1)]
                ; MV.modify grown (Map.insert key val) idx
-               ; writeSTRef mmref grown
+               ; writeIORef mmref grown
                }
 
--- insert a pair (key, value) into the MapMap
+-- insert a pair (key, value) into the IOMapMap
 -- ensures: it uses the supplied combining function if the mapping is already present
-insertWith :: (Ord k) => STRef s (MapMap s k v) -> Int -> (v -> v -> v) -> k -> v -> ST.ST s ()
+insertWith :: (Ord k) => IORef (IOMapMap k v) -> Int -> (v -> v -> v) -> k -> v -> IO ()
 insertWith mmref idx f key val = do
-  mm <- readSTRef mmref
+  mm <- readIORef mmref
   let len = MV.length mm
   if idx < len
     then MV.unsafeModify mm (Map.insertWith f key val) idx
@@ -57,33 +62,50 @@ insertWith mmref idx f key val = do
          in do { grown <- MV.grow mm (newLen-len)
                ; mapM_ (\i -> MV.unsafeWrite grown i Map.empty) [len..(newLen-1)]
                ; MV.unsafeModify grown (Map.insert key val) idx
-               ; writeSTRef mmref grown
+               ; writeIORef mmref grown
                }
 
-lookup :: STRef s (MapMap s k v) -> Int -> ST.ST s [(k,v)]
+lookup :: IORef (IOMapMap k v) -> Int -> IO [(k,v)]
 lookup mmref idx = do
-  mm <- readSTRef mmref
+  mm <- readIORef mmref
   if idx < MV.length mm
     then Map.toList <$> MV.read mm idx
     else return []
 
+lookupValue :: (Ord k) => IORef (IOMapMap k v) -> Int -> k -> IO (Maybe v)
+lookupValue mmref idx mapIdx = do
+  mm <- readIORef mmref
+  if idx < MV.length mm
+    then Map.lookup mapIdx <$> MV.read mm idx
+    else return Nothing
+ 
+delete :: Ord k => IORef (IOMapMap k v) -> (Int, k) -> IO ()
+delete mmref (idx, mapIdx) = do
+  mm <- readIORef mmref
+  when (idx < MV.length mm) $ MV.unsafeModify mm (Map.delete mapIdx) idx
+
 -- check the presence of the key in the Map at StateId position
-member :: (Ord k) => STRef s (MapMap s k v) -> Int -> k -> ST.ST s Bool
+member :: (Ord k) => IORef (IOMapMap k v) -> Int -> k -> IO Bool
 member mmref idx key = do
-  mm <- readSTRef mmref
+  mm <- readIORef mmref
   if idx < MV.length mm
     then Map.member key <$> MV.unsafeRead mm idx
     else return False
 
 -- an empty Map Map, an array of maps
-empty :: ST.ST s (STRef s (MapMap s k v))
+empty :: IO (IORef (IOMapMap k v))
 empty = do
   mm <- MV.replicate 4 Map.empty
-  newSTRef mm
+  newIORef mm
+
+foldMaps :: (Ord k) => IORef (IOMapMap k v) -> IO (Map (Int,k) v)
+foldMaps mmref = do 
+  mm <- readIORef mmref 
+  MV.ifoldl' (\acc idx m -> Map.union acc . Map.mapKeys (\k -> (idx, k)) $ m) Map.empty mm
 
 -- for debugging purposes
-showMapMap :: (Show  k, Show v) => MapMap s k v -> ST.ST s String
-showMapMap = MV.ifoldl'
+showIOMapMap :: (Show  k, Show v) => IOMapMap k v -> IO String
+showIOMapMap = MV.ifoldl'
     (\acc idx el -> acc ++ "Map at position " ++ show idx ++ " : " ++ show el ++ "\n\n")
     ""
 
