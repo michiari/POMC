5ebe2baa07a8d5fe9539ce0ffa91b137e4d69af1
Francesco Pontiggia <francesco.pontiggia@mail.polimi.it>
2025-04-12 12:10:16 +0100

    update probabilistic experiments

--- eval/prob/Sherwood/generate.py
+++ eval/prob/Sherwood/generate.py
@@ -3,6 +3,7 @@ import fileinput
 import math
 import os
 
+term_benchmark = [('', "term")]
 
 qual_benchmark = [
     ("G ((call And B And sorted And valOccurs And [B|left <= right]) --> XNu correctIndex);\n// S1: Partial Correctness", "S1"), # partial correctness
@@ -24,8 +25,9 @@ quant_benchmark = [
     #("G ((call And B And sorted And (~ valOccurs) And [B|left < right]) --> XNd (call And B));\n// S4: Stack inspection (v2)", "S6"), # stack inspection (v2)
     ]
 
-queries = [('qualitative',5,8, qual_benchmark), ('quantitative',5,4, quant_benchmark)]
-
+queries = [ ('qualitative',[(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7), (2,1), (2,2), (2,3), (2,4), (3,1), (3,2), (4,1), (4,2)], qual_benchmark)
+          , ('approximate',[(1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7), (2,1), (2,2), (2,3), (2,4), (3,1), (3,2), (4,1), (4,2)], term_benchmark)
+          , ('quantitative',[(1,1), (1,2), (1,3), (2,1), (2,2), (3,1), (4,1)],quant_benchmark)]
 
 def array_domain_comment(bits):
     return '// Elements domain bits: K = ' + str(bits) + ';'
@@ -40,32 +42,37 @@ def array_length_assignment(len):
     return 'max_array_index = ' + str(len -1) + 'u4;'
 
 def build_query(query, exp):
-    return 'probabilistic query: ' + query + ';\nformula = ' + exp
+    if exp:
+        return 'probabilistic query: ' + query + ';\nformula = ' + exp
+    else: return 'probabilistic query: ' + query + ';\n'
 
 # model file
 filein = 'Sherwood.txt'
 
-for (query, k_bound, m_bound, bench) in queries:
+# remove benchmark folder if already present
+if os.path.isfile('benchmark/'):
+    os.remove('benchmark/')
+
+for (query, valuations, bench) in queries:
     for (exp, name) in bench:
         prob_query = build_query(query, exp)
-        for u_size in range(1,k_bound):
+        for (u_size, arr_size) in valuations:
             arr_domain_comm = array_domain_comment(u_size)
             arr_domain_ass = array_domain_assignment(u_size)
-            for arr_size in range(1,m_bound):
-                arr_len_comm = array_length_comment(arr_size)
-                arr_len_ass = array_length_assignment(arr_size)
-                fileout = 'benchmark/' + query + '/' + name + '/sherwood-' + str(u_size) + '.' + str(arr_size) + '.' + name + '.pomc'
-                with open(filein, 'r') as f1:
-                    os.makedirs(os.path.dirname(fileout), exist_ok=True)
-                    with open(fileout, 'w+') as f2:
-                        for line in f1:
-                            f2.write(
-                                line.replace('probabilistic query;', prob_query) # adding the formula
-                                    .replace('// Elements\' domain bits: K = *;', arr_domain_comm) # fixing the domain of array values
-                                    .replace('max_array_value = *;', arr_domain_ass)
-                                    .replace('// Array length: M = *;', arr_len_comm) # fixing array length
-                                    .replace('max_array_index = *;', arr_len_ass)
-                                )
+            arr_len_comm = array_length_comment(arr_size)
+            arr_len_ass = array_length_assignment(arr_size)
+            fileout = 'benchmark/' + query + '/' + name + '/sherwood-' + str(u_size) + '.' + str(arr_size) + '.' + name + '.pomc'
+            with open(filein, 'r') as f1:
+                os.makedirs(os.path.dirname(fileout), exist_ok=True)
+                with open(fileout, 'w+') as f2:
+                    for line in f1:
+                        f2.write(
+                            line.replace('probabilistic query;', prob_query) # adding the formula
+                                .replace('// Elements\' domain bits: K = *;', arr_domain_comm) # fixing the domain of array values
+                                .replace('max_array_value = *;', arr_domain_ass)
+                                .replace('// Array length: M = *;', arr_len_comm) # fixing array length
+                                .replace('max_array_index = *;', arr_len_ass)
+                            )
                             
 # Checking Sherwood properties with Storm
 # Q19: storm --explicit sherwood-1.2.Q20.tra sherwood-1.2.Q20.lab --prop "P=? [G ((\"call\" & \"b\" & \"sorted\" & \"valoccurs\" & \"bleftright\") => X (F (\"call\" & \"b\")))]"
