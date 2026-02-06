f4da9a8cbce401f8607308b1fdd27d2e39943c26
Francesco Pontiggia <francesco.pontiggia@mail.polimi.it>
2026-02-06 14:11:41 +0100

    Refactor construction of the support graph; more explicit representation of outgoing edges from a semiconfiguration; keep track explicitly whether a semiconf performs push, shift, or pop transitions.

--- src/Pomc/Prob/SupportGraph.hs
+++ src/Pomc/Prob/SupportGraph.hs
@@ -1,14 +1,15 @@
 {-# LANGUAGE TupleSections #-}
 {- |
    Module      : Pomc.Prob.SupportGraph
-   Copyright   : 2023-2025 Francesco Pontiggia
+   Copyright   : 2023-2026 Francesco Pontiggia
    License     : MIT
    Maintainer  : Francesco Pontiggia
 -}
 
 module Pomc.Prob.SupportGraph ( SupportGraph
-                              , buildSupportGraph
                               , GraphNode(..)
+                              , TransitionInfo(..)
+                              , buildSupportGraph
                               ) where
 import Pomc.Prob.ProbUtils
 import Pomc.SatUtil(freshPosId)
@@ -17,19 +18,15 @@ import Pomc.Prec (Prec(..))
 import qualified Pomc.CustoMap as CM
 import Pomc.SetMap(SetMap)
 import qualified Pomc.SetMap as SM
-
 import Data.Vector (Vector)
 import qualified Data.Vector as V
-
 import qualified Data.Set as Set
 import Data.IntSet(IntSet)
 import qualified Data.IntSet as IntSet
-
-import qualified Data.Strict.IntMap as StrictIntMap
-import Data.Strict.IntMap(IntMap)
 import Data.Strict.Map(Map)
-
 import qualified Data.IntMap as IntMap
+import Data.Hashable (Hashable)
+import qualified Data.HashTable.ST.Basic as BH
 
 import Control.Monad(when)
 import Data.Bifunctor (first)
@@ -37,19 +34,15 @@ import Control.Monad.ST (ST)
 import Data.STRef (STRef, newSTRef, readSTRef, modifySTRef')
 import Data.Maybe (fromJust, isNothing, mapMaybe)
 
-import Data.Hashable (Hashable)
-import qualified Data.HashTable.ST.Basic as BH
+-- information about successor semiconfigurations
+data TransitionInfo = Push IntSet (IntMap.IntMap Prob) | Shift (IntMap.IntMap Prob) | Pop (IntMap.IntMap Prob)
+  deriving Show
 
 -- a node in the support graph, corresponding to a semiconfiguration
 data GraphNode state = GraphNode
-  { gnId   :: Int
-  , semiconf   :: (StateId state, Stack state)
-  -- if the semiconf is a pop one, these IntMap and IntSet are empty
-  , internalEdges :: IntMap Prob
-  , supportEdges  :: IntSet
-  -- if the semiconf is a pop one, then popContexts represents the probability distribution of the pop transition over "return states", and not semiconfs
-  -- otherwise this IntMap is empty
-  , popContexts :: IntMap.IntMap Prob
+  { gnId     :: Int
+  , semiconf :: (StateId state, Stack state)
+  , gnEdges  :: TransitionInfo
   } deriving Show
 
 instance Eq (GraphNode state) where
@@ -73,13 +66,33 @@ data Globals s state = Globals
   , graph      :: STRef s (PartialSupportGraph s state)
   }
 
+lookupId :: (Eq state, Hashable state, Show state)
+  => Globals s state
+  -> (Prob, (StateId state, Stack state))
+  -> ST s (Int, Prob, Maybe (StateId state, Stack state))
+lookupId globals (prob_, dest) = do
+  maybeId <- BH.lookup (graphMap globals) (decode dest)
+  actualId <- maybe (freshPosId $ idSeq globals) return maybeId
+  when (isNothing maybeId) $ BH.insert (graphMap globals) (decode dest) actualId
+  return (actualId, prob_, if isNothing maybeId then Just dest else Nothing)
+
+lookupIdSupp :: (Eq state, Hashable state, Show state)
+  => Globals s state
+  -> (StateId state, Stack state)
+  -> ST s (Int, Maybe (StateId state, Stack state))
+lookupIdSupp globals dest = do
+  maybeId <- BH.lookup (graphMap globals) (decode dest)
+  actualId <- maybe (freshPosId $ idSeq globals) return maybeId
+  when (isNothing maybeId) $ BH.insert (graphMap globals) (decode dest) actualId
+  return (actualId, if isNothing maybeId then Just dest else Nothing)
+
 -- build the support graph of an input pOPA
 buildSupportGraph  :: (Ord state, Hashable state, Show state)
         => DeltaWrapper state -- probabilistic delta relation of a popa
         -> (state, Label) -- (initial state of the popa, label of the initial state)
         -> STRef s Stats
         -> ST s (SupportGraph state, SidMap state) -- returning a graph
-buildSupportGraph probdelta (i, iLabel) stats = do
+buildSupportGraph probDelta (i, iLabel) stats = do
   -- initialize the global variables
   newSig <- initSIdGen
   emptySuppStarts <- SM.empty
@@ -91,7 +104,6 @@ buildSupportGraph probdelta (i, iLabel) stats = do
   emptyGraph <- CM.empty
   initialId <- freshPosId newIdSequence
   BH.insert emptyGraphMap (decode initialNode) initialId
-  CM.insert emptyGraph initialId $ GraphNode {gnId=initialId, semiconf=initialNode, internalEdges= StrictIntMap.empty, supportEdges = IntSet.empty, popContexts = IntMap.empty}
   let globals = Globals { sIdGen = newSig
                         , idSeq = newIdSequence
                         , graphMap = emptyGraphMap
@@ -100,11 +112,10 @@ buildSupportGraph probdelta (i, iLabel) stats = do
                         , graph = emptyGraph
                         }
   -- compute the support graph of the input popa
-  build globals probdelta initialNode
+  build globals probDelta (initialId, initialNode)
   idx <- readSTRef . idSeq $ globals
   statesCount <- sIdCount newSig
-  modifySTRef' stats $ \s -> s{suppGraphLen = idx}
-  modifySTRef' stats $ \s -> s{popaStatesCount = statesCount}
+  modifySTRef' stats $ \s -> s{suppGraphLen = idx, popaStatesCount = statesCount}
   suppGraph <- V.freeze . CM.take idx =<< (readSTRef . graph $ globals)
   sidMap <- sIdMap (sIdGen globals)
   return (suppGraph, sidMap)
@@ -112,25 +123,25 @@ buildSupportGraph probdelta (i, iLabel) stats = do
 build :: (Eq state, Hashable state, Show state)
       => Globals s state -- global variables of the algorithm
       -> DeltaWrapper state -- delta relation of the popa
-      -> (StateId state, Stack state) -- current semiconfiguration
+      -> (Int, (StateId state, Stack state)) -- current semiconfiguration
       -> ST s ()
-build globals probdelta (q,g) = do
+build globals probDelta (scId_, (q,g)) =
   let qLabel = getLabel q
       qState = getState q
-      precRel = (prec probdelta) (fst . fromJust $ g) qLabel
+      precRel = (prec probDelta) (fst . fromJust $ g) qLabel
       cases
         -- this case includes the initial push
         | (isNothing g) || precRel == Just Yield =
-          buildPush globals probdelta q g qState qLabel
+          buildPush globals probDelta q g qState qLabel scId_
 
         | precRel == Just Equal =
-          buildShift globals probdelta q g qState qLabel
+          buildShift globals probDelta q g qState qLabel scId_
 
         | precRel == Just Take =
-          buildPop globals probdelta q g qState
+          buildPop globals probDelta q g qState scId_
 
         | otherwise = error "unexpected prec rel."
-  cases
+  in cases
 
 buildPush :: (Eq state, Hashable state, Show state)
           => Globals s state
@@ -139,17 +150,32 @@ buildPush :: (Eq state, Hashable state, Show state)
           -> Stack state
           -> state
           -> Label
+          -> Int
           -> ST s ()
-buildPush globals probdelta q g qState qLabel =
+buildPush globals probDelta q g qState qLabel scId_ =
   let wrapPush (p, pLabel, prob_) = do
         newState <- wrapState (sIdGen globals) p pLabel
         return (prob_, (newState,Just (qLabel, q)))
   in do
     SM.insert (suppStarts globals) (getId q) g
-    pushEdges <- mapM wrapPush $ (deltaPush probdelta) qState
-    buildInternalTransitions globals probdelta (q,g) pushEdges
+    pushEdges <- mapM wrapPush $ (deltaPush probDelta) qState
     suppEdges <- map (,g) . Set.toList <$> SM.lookup (suppEnds globals) (getId q)
-    buildSupportTransitions globals probdelta (q,g) suppEdges
+    pushSuccs <- mapM (lookupId globals) pushEdges
+    suppSuccs <- mapM (lookupIdSupp globals) suppEdges
+    let -- we use sum here to handle non normalized probability distributions 
+        -- (i.e., multiple probabilities to go to the same state, that have to be summed)
+        pushMap = IntMap.fromListWith (+) . map (\(id_, prob_, _) -> (id_, prob_)) $ pushSuccs
+        suppSet = IntSet.fromList . map fst $ suppSuccs
+        pushInfo = Push suppSet pushMap
+    -- adding current Push semiconf to the Support Graph
+    CM.insert (graph globals) scId_
+      $ GraphNode {gnId=scId_, semiconf=(q,g), gnEdges = pushInfo}
+    -- exploring push transitions of the Support Graph
+    mapM_ (build globals probDelta) 
+      $ mapMaybe (\(id_,_, maybeDest) -> fmap (id_,) maybeDest) pushSuccs
+    -- exploring supp transitions of the Support Graph
+    mapM_ (build globals probDelta) 
+      $ mapMaybe (\(id_,maybeDest) -> fmap (id_,) maybeDest) suppSuccs
 
 buildShift :: (Eq state, Hashable state, Show state)
            => Globals s state
@@ -158,14 +184,23 @@ buildShift :: (Eq state, Hashable state, Show state)
            -> Stack state
            -> state
            -> Label
+           -> Int
            -> ST s ()
-buildShift globals probdelta q g qState qLabel =
+buildShift globals probDelta q g qState qLabel scId_ =
   let wrapShift (p, pLabel, prob_)= do
         newState <- wrapState (sIdGen globals) p pLabel
         return (prob_, (newState, Just (qLabel, snd . fromJust $ g)))
   in do
-    shiftEdges <- mapM wrapShift $ (deltaShift probdelta) qState
-    buildInternalTransitions globals probdelta (q,g) shiftEdges
+    shiftEdges <- mapM wrapShift $ (deltaShift probDelta) qState
+    shiftSuccs <- mapM (lookupId globals) shiftEdges
+    let -- we use sum here to handle non normalized probability distributions 
+        -- (i.e., multiple probabilities to go to the same state, that have to be summed)
+        shiftInfo = Shift (IntMap.fromListWith (+) . map (\(id_, prob_, _) -> (id_, prob_)) $ shiftSuccs)
+    -- adding current Shift semiconf to the Support Graph
+    CM.insert (graph globals) scId_
+      $ GraphNode {gnId=scId_, semiconf=(q,g), gnEdges = shiftInfo}
+    -- exploring Shift transitions of the Support Graph
+    mapM_ (build globals probDelta) $ mapMaybe (\(id_,_, maybeDest) -> fmap (id_,) maybeDest) shiftSuccs
 
 buildPop :: (Eq state, Hashable state, Show state)
          => Globals s state
@@ -173,82 +208,31 @@ buildPop :: (Eq state, Hashable state, Show state)
          -> StateId state
          -> Stack state
          -> state
+         -> Int
          -> ST s ()
-buildPop globals probdelta q g qState =
-  let r = snd . fromJust $ g
+buildPop globals probDelta q g qState scId_ =
+  let addSupp suppId_ g@GraphNode{gnEdges = Push suppSet pushMap} = 
+        g{gnEdges = Push (IntSet.insert suppId_ suppSet) pushMap}
+      r = snd . fromJust $ g
       wrapPop (p, pLabel, prob_) = do
         newState <- wrapState (sIdGen globals) p pLabel
         return (newState, prob_)
       doPop (newState, _) =
-        let closeSupports g' = buildSupportTransitions globals probdelta (r,g') [(newState, g')]
+        let closeSupports g' = do 
+              fromId <- fromJust <$> BH.lookup (graphMap globals) (decode (r,g'))
+              suppSucc <- lookupIdSupp globals (newState, g')
+              CM.modify (graph globals) (addSupp (fst suppSucc)) fromId
+              case suppSucc of 
+                (suppId, Just dest) -> build globals probDelta (suppId, dest)
+                _ -> return ()
         in do
           SM.insert (suppEnds globals) (getId r) newState
           currentSuppStarts <- SM.lookup (suppStarts globals) (getId r)
-          mapM_ (closeSupports) currentSuppStarts
+          mapM_ closeSupports currentSuppStarts
   in do
-    popCntxs <- mapM wrapPop $ (deltaPop probdelta) qState (getState . snd . fromJust $ g)
-    addPopContexts globals (q,g) popCntxs
+    popCntxs <- mapM wrapPop $ (deltaPop probDelta) qState (getState . snd . fromJust $ g)
+    -- adding current Pop semiconf to the Support Graph
+    let popInfo = Pop (IntMap.fromListWith (+) $ map (first getId) popCntxs)
+    CM.insert (graph globals) scId_
+      $ GraphNode {gnId=scId_, semiconf=(q,g), gnEdges = popInfo}
     mapM_ doPop popCntxs
-
---
--- functions that modify the stored support graph
---
-
--- add right contexts to a pop semiconfiguration
-addPopContexts :: (Eq state, Hashable state, Show state)
-                => Globals s state
-                -> (StateId state, Stack state) -- from state 
-                -> [(StateId state, Prob)]
-                -> ST s ()
-addPopContexts globals from rCntxs =
-  let
-    -- we use unionWith (+) and fromListWith (+) because the input distribution might not be normalized - i.e., there might be duplicate pop transitions
-    insertContext g@GraphNode{popContexts= cntxs} =g{popContexts = IntMap.unionWith (+) cntxs (IntMap.fromListWith (+) $ map (first getId) rCntxs)}
-  in BH.lookup (graphMap globals) (decode from) >>= CM.modify (graph globals) insertContext . fromJust
-
--- decomposing transitions of a semiconf
-buildInternalTransitions :: (Eq state, Hashable state, Show state)
-                 => Globals s state
-                 -> DeltaWrapper state
-                 -> (StateId state, Stack state) -- from semiconf 
-                 -> [(Prob, (StateId state, Stack state))] -- Push/Shift transitions
-                 -> ST s ()
-buildInternalTransitions globals probdelta from intDests =
-  let
-    computeId (prob_, dest) = do
-      maybeId <- BH.lookup (graphMap globals) (decode dest)
-      actualId <- maybe (freshPosId $ idSeq globals) return maybeId
-      when (isNothing maybeId) $ do
-          BH.insert (graphMap globals) (decode dest) actualId
-          CM.insert (graph globals) actualId $ GraphNode {gnId=actualId, semiconf=dest, internalEdges= StrictIntMap.empty, supportEdges = IntSet.empty, popContexts = IntMap.empty}
-      return (actualId, prob_, if isNothing maybeId then Just dest else Nothing)
-  in do
-    intEdges <- mapM computeId intDests
-    let -- we use sum here to handle non normalized probability distributions (i.e., multiple probabilities to go to the same state, that have to be summed)
-        intEdgs = StrictIntMap.fromListWith (+) . map (\(id_, prob_, _) -> (id_, prob_)) $ intEdges
-    fromId <- fromJust <$> BH.lookup (graphMap globals) (decode from)
-    CM.modify (graph globals) (\g@GraphNode{internalEdges = intEdges_} -> g{internalEdges = StrictIntMap.unionWith (+) intEdges_ intEdgs}) fromId
-    mapM_ (build globals probdelta) $ mapMaybe (\(_,_, maybeDest) -> maybeDest) intEdges
-
--- decomposing transitions of a semiconf
-buildSupportTransitions :: (Eq state, Hashable state, Show state)
-                 => Globals s state
-                 -> DeltaWrapper state
-                 -> (StateId state, Stack state) -- from semiconf 
-                 -> [(StateId state, Stack state)] -- support transitions
-                 -> ST s ()
-buildSupportTransitions globals probdelta from suppDests =
-  let
-    computeId dest = do
-      maybeId <- BH.lookup (graphMap globals) (decode dest)
-      actualId <- maybe (freshPosId $ idSeq globals) return maybeId
-      when (isNothing maybeId) $ do
-          BH.insert (graphMap globals) (decode dest) actualId
-          CM.insert (graph globals) actualId $ GraphNode {gnId=actualId, semiconf=dest, internalEdges= StrictIntMap.empty, supportEdges = IntSet.empty, popContexts = IntMap.empty}
-      return (actualId, if isNothing maybeId then Just dest else Nothing)
-  in do
-    suppEdges <- mapM computeId suppDests
-    let suppEds = IntSet.fromList . map fst $ suppEdges
-    fromId <- fromJust <$> BH.lookup (graphMap globals) (decode from)
-    CM.modify (graph globals) (\g@GraphNode{supportEdges = suppEdges_} -> g{supportEdges = IntSet.union suppEdges_ suppEds}) fromId
-    mapM_ (build globals probdelta) $ mapMaybe snd suppEdges
