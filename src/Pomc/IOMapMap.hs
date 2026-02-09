cd8b664f1127099949927dc808c83770106d4534
Francesco Pontiggia <francesco.pontiggia@mail.polimi.it>
2026-02-09 14:31:25 +0100

    Refactor IOMapMap data structure.

--- src/Pomc/IOMapMap.hs
+++ src/Pomc/IOMapMap.hs
@@ -1,6 +1,6 @@
 {- |
    Module      : Pomc.IOIOMapMap
-   Copyright   : 2024-2025 Francesco Pontiggia
+   Copyright   : 2024-2026 Francesco Pontiggia
    License     : MIT
    Maintainer  : Francesco Pontiggia
 -}
@@ -18,24 +18,21 @@ module Pomc.IOMapMap ( IOMapMap
                    , empty
                    , emptySized
                    , showIOMapMap
-                   , foldMaps
+                   , values
+                   , valuesWith
                    ) where
 
 import Prelude hiding (lookup)
-
 import qualified Data.Vector.Mutable as MV
-
-import qualified Data.IntMap as Map
 import Data.IntMap(IntMap)
-
-import Data.Map(Map)
-import qualified Data.Map as GM
-import Data.IORef (readIORef, IORef, newIORef, writeIORef)
-import Data.Vector.Mutable (IOVector)
-
-import Control.Monad(when)
+import qualified Data.IntMap as IntMap
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as IntSet
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Data.IORef (readIORef, IORef, newIORef, writeIORef)
+import Data.Vector.Mutable (IOVector)
+import Control.Monad(when)
 
 -- Map to Maps
 type IOMapMap v = IOVector (IntMap v)
@@ -47,13 +44,13 @@ insert mmref idx key val = do
   mm <- readIORef mmref
   let len = MV.length mm
   if idx < len
-    then MV.unsafeModify mm (Map.insert key val) idx
+    then MV.unsafeModify mm (IntMap.insert key val) idx
     else let newLen = computeLen len idx
              computeLen size newIdx | newIdx < size = size
                                     | otherwise = computeLen (size*2) newIdx
          in do { grown <- MV.grow mm (newLen-len)
-               ; mapM_ (\i -> MV.unsafeWrite grown i Map.empty) [len..(newLen-1)]
-               ; MV.modify grown (Map.insert key val) idx
+               ; mapM_ (\i -> MV.unsafeWrite grown i IntMap.empty) [len..(newLen-1)]
+               ; MV.unsafeModify grown (IntMap.insert key val) idx
                ; writeIORef mmref grown
                }
 
@@ -65,13 +62,13 @@ insertMap mmref idx m = do
   let len = MV.length mm
   if idx < len
     -- union is left-biased, so we prefer new values to old ones in case of overlapping keys
-    then MV.unsafeModify mm (Map.union m) idx
+    then MV.unsafeModify mm (IntMap.union m) idx
     else let newLen = computeLen len idx
              computeLen size newIdx | newIdx < size = size
                                     | otherwise = computeLen (size*2) newIdx
          in do { grown <- MV.grow mm (newLen-len)
-               ; mapM_ (\i -> MV.unsafeWrite grown i Map.empty) [len..(newLen-1)]
-               ; MV.unsafeModify grown (Map.union m) idx
+               ; mapM_ (\i -> MV.unsafeWrite grown i IntMap.empty) [len..(newLen-1)]
+               ; MV.unsafeModify grown (IntMap.union m) idx
                ; writeIORef mmref grown
                }
 
@@ -82,13 +79,13 @@ insertWith mmref idx f key val = do
   mm <- readIORef mmref
   let len = MV.length mm
   if idx < len
-    then MV.unsafeModify mm (Map.insertWith f key val) idx
+    then MV.unsafeModify mm (IntMap.insertWith f key val) idx
     else let newLen = computeLen len idx
              computeLen size newIdx | newIdx < size = size
                                     | otherwise = computeLen (size*2) newIdx
          in do { grown <- MV.grow mm (newLen-len)
-               ; mapM_ (\i -> MV.unsafeWrite grown i Map.empty) [len..(newLen-1)]
-               ; MV.unsafeModify grown (Map.insert key val) idx
+               ; mapM_ (\i -> MV.unsafeWrite grown i IntMap.empty) [len..(newLen-1)]
+               ; MV.unsafeModify grown (IntMap.insert key val) idx
                ; writeIORef mmref grown
                }
 
@@ -96,7 +93,7 @@ lookup :: IORef (IOMapMap v) -> Int -> IO [(Int,v)]
 lookup mmref idx = do
   mm <- readIORef mmref
   if idx < MV.length mm
-    then Map.toList <$> MV.unsafeRead mm idx
+    then IntMap.toList <$> MV.unsafeRead mm idx
     else return []
 
 lookupMap :: IORef (IOMapMap v) -> Int -> IO (IntMap v)
@@ -104,55 +101,59 @@ lookupMap mmref idx = do
   mm <- readIORef mmref
   if idx < MV.length mm
     then MV.unsafeRead mm idx
-    else return Map.empty
+    else return IntMap.empty
 
 lookupKeys :: IORef (IOMapMap v) -> Int -> IO IntSet
 lookupKeys mmref idx = do
   mm <- readIORef mmref
   if idx < MV.length mm
-    then Map.keysSet <$> MV.unsafeRead mm idx
+    then IntMap.keysSet <$> MV.unsafeRead mm idx
     else return IntSet.empty
 
 lookupValue :: IORef (IOMapMap v) -> Int -> Int -> IO (Maybe v)
 lookupValue mmref idx mapIdx = do
   mm <- readIORef mmref
   if idx < MV.length mm
-    then Map.lookup mapIdx <$> MV.unsafeRead mm idx
+    then IntMap.lookup mapIdx <$> MV.unsafeRead mm idx
     else return Nothing
 
 delete :: IORef (IOMapMap v) -> Int -> Int -> IO ()
 delete mmref idx mapIdx = do
   mm <- readIORef mmref
-  when (idx < MV.length mm) $ MV.unsafeModify mm (Map.delete mapIdx) idx
+  when (idx < MV.length mm) $ MV.unsafeModify mm (IntMap.delete mapIdx) idx
 
 -- check the presence of the key in the Map at StateId position
 member :: IORef (IOMapMap v) -> Int -> Int -> IO Bool
 member mmref idx key = do
   mm <- readIORef mmref
   if idx < MV.length mm
-    then Map.member key <$> MV.unsafeRead mm idx
+    then IntMap.member key <$> MV.unsafeRead mm idx
     else return False
 
 -- an empty Map Map, an array of maps
 empty :: IO (IORef (IOMapMap v))
 empty = do
-  mm <- MV.replicate 4 Map.empty
+  mm <- MV.replicate 4 IntMap.empty
   newIORef mm
 
 -- an empty Map Map, an array of maps
 emptySized :: Int -> IO (IORef (IOMapMap v))
 emptySized l = do
-  mm <- MV.replicate l Map.empty
+  mm <- MV.replicate l IntMap.empty
   newIORef mm
 
-foldMaps :: IORef (IOMapMap v) -> IO (Map (Int,Int) v)
-foldMaps mmref = do
+values :: IORef (IOMapMap v) -> IO (Vector [v])
+values mmref = do
   mm <- readIORef mmref
-  MV.ifoldl' (\acc idx m -> GM.union acc . GM.fromList . map (\(k, i) -> ((idx, k), i)) . Map.toList $ m) GM.empty mm
+  V.map IntMap.elems <$> V.freeze mm
+
+valuesWith :: IORef (IOMapMap v) -> (v -> w) -> IO (Vector [w])
+valuesWith mmref f = do
+  mm <- readIORef mmref
+  V.map (map f . IntMap.elems) <$> V.freeze mm
 
 -- for debugging purposes
 showIOMapMap :: (Show v) => IOMapMap v -> IO String
 showIOMapMap = MV.ifoldl'
     (\acc idx el -> acc ++ "Map at position " ++ show idx ++ " : " ++ show el ++ "\n\n")
     ""
-
